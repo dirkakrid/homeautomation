e44ac334682fa0aa65e213e367d3019013b2007b
Wojciech Bederski <github@wuub.net>
2013-11-03 16:45:53 +0100

    switch ir lights to api

--- lights/lights.py
+++ lights/lights.py
@@ -2,48 +2,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import with_statement, division, absolute_import, print_function
 
-import time
-import atexit
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(15, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(18, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(22, GPIO.OUT, initial=GPIO.HIGH)
-atexit.register(GPIO.cleanup)
+import requests
 
 from irapp import IrApp
 app = IrApp(__name__)
 
-def click(no):
-    for _ in range(2):
-        GPIO.output(no, 0)
-        try:
-            time.sleep(0.1)
-        finally:
-            GPIO.output(no, 1)
-        time.sleep(0.2)
-
-
 @app.route("KEY_VOLUMEUP", remote="lights")
 def key_1():
-    click(18)
-
+    requests.get("http://127.0.0.1/api/lights/small/on")
 
 @app.route("KEY_VOLUMEDOWN", remote="lights")
 def key_2():
-    click(22)
-
+    requests.get("http://127.0.0.1/api/lights/small/off")
 
 @app.route("KEY_UP", remote="lights")
 def key_LEFT():
-    click(11)
-
+    requests.get("http://127.0.0.1/api/lights/large/on")
 
 @app.route("KEY_DOWN", remote="lights")
 def key_UP():
-    click(15)
+    requests.get("http://127.0.0.1/api/lights/large/off")
 
 if __name__ == '__main__':
     app.run()
